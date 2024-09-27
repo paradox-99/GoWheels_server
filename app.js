bec95c842a03446c84fe040d05d89ee3e40274cd
Radwanul Islam Nayeem <103718355+paradox-99@users.noreply.github.com>
2024-09-27 09:50:40 +0600

    added jwt

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+const authRoutes = require('./routes/authRoutes')
 const userRoutes = require('./routes/userRoutes');
 const carRoute = require('./routes/carRoutes');
 const reviewsAndRatingsRoute = require('./routes/reviewAndRatingRoutes');
@@ -13,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-
+app.use('/api/authorization', authRoutes);
 app.use('/api/usersRoute', userRoutes);
 app.use('/api/carsRoute', carRoute);
 app.use('/api/reviewsRoute', reviewsAndRatingsRoute);
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -12,4 +12,18 @@ const showUsers = async(req, res) => {
     }
 }
 
-module.exports = {showUsers}
+const getUser = async(req, res) => {
+    try{
+        const db = await connectDB();
+        const collection = db.collection('users');
+        const email = req.params.email;
+        const query = { "email":  email}
+        const user = await collection.find(query).toArray();
+        res.send(user);
+    }
+    catch(error){
+        res.status(500).send( 'Error retrieving user');
+    }
+}
+
+module.exports = { showUsers, getUser }
