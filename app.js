3ce485eea7dd7a0c97384547b4ba3982c0fda2ab
theabsparrow <absbashar04@gmail.com>
2024-09-30 11:33:25 +0600

    authentication done

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/authRoutes')
 const userRoutes = require('./routes/userRoutes');
 const carRoute = require('./routes/carRoutes');
 const reviewsAndRatingsRoute = require('./routes/reviewAndRatingRoutes');
 const agency = require('./routes/agencyRoutes');
-const userBookingsRoutes = require('./routes/userBookingRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
 
 app.use('/api/authorization', authRoutes);
 app.use('/api/usersRoute', userRoutes);
 app.use('/api/carsRoute', carRoute);
 app.use('/api/reviewsRoute', reviewsAndRatingsRoute);
 app.use('/api/agencyRoute', agency);
-app.use('/api/bookings', userBookingsRoutes);
 
 module.exports = app;
