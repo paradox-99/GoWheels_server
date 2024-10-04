67af3f219327f4aa12e85e0e13c027ecccfb34e1
Masum Billah <masumbillah2062003@gmail.com>
2024-10-04 18:50:13 +0600

    agency approve and reject work complete

--- controllers/agencyControllers.js
+++ controllers/agencyControllers.js
@@ -1,29 +1,71 @@
 const connectDB = require("../config/db")
+const { ObjectId } = require('mongodb')
 
-const showAgency = async(req, res) => {
-    try{
+const showAgency = async (req, res) => {
+    try {
         const db = await connectDB();
         const collection = db.collection('agencyData');
         const agencyData = await collection.find().toArray();
         res.send(agencyData);
     }
-    catch(error) {
-        res.status(500).send( 'Error retrieving reviews');
+    catch (error) {
+        res.status(500).send('Error retrieving reviews');
     }
 }
 
-const getAgency = async(req, res) => {
-    try{
+const getAgency = async (req, res) => {
+    try {
         const db = await connectDB();
         const collection = db.collection('agencyData');
         const agencyId = req.params.agencyId;
-        const query = { "agency_id":  agencyId}
+        const query = { "agency_id": agencyId }
         const agency = await collection.find(query).toArray();
         res.send(agency);
     }
-    catch(error){
-        res.status(500).send( 'Error retrieving agency');
+    catch (error) {
+        res.status(500).send('Error retrieving agency');
     }
 }
 
-module.exports = { showAgency , getAgency }
+const approveAgency = async (req, res) => {
+    try {
+        const db = await connectDB();
+        const collection = db.collection("agencyData");
+        const agencyId = req.params.agencyId
+        const filter = { _id: new ObjectId(agencyId) }
+        const updateDoc = {
+            $set: {
+                status: "verified",
+                userRole: "agency",
+            }
+        }
+        const result = await collection.updateOne(filter, updateDoc)
+        res.send(result)
+    }
+    catch (error) {
+        res.status(500).send('Error retrieving agency');
+    }
+}
+const rejectAgency = async (req, res) => {
+    try {
+        const db = await connectDB();
+        const collection = db.collection("agencyData");
+        const agencyId = req.params.agencyId
+        const filter = { _id: new ObjectId(agencyId) }
+        const updateDoc = {
+            $set: {
+                status: "Rejected",
+                userRole: "user",
+            }
+        }
+        const result = await collection.updateOne(filter, updateDoc)
+        res.send(result)
+    }
+    catch (error) {
+        res.status(500).send('Error retrieving agency');
+    }
+}
+
+
+
+module.exports = { showAgency, getAgency, approveAgency, rejectAgency }
--- routes/agencyRoutes.js
+++ routes/agencyRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { showAgency, getAgency } = require('../controllers/agencyControllers')
+const { showAgency, getAgency, approveAgency, rejectAgency } = require('../controllers/agencyControllers')
 const Router = express.Router();
 
 Router.get('/agency', showAgency);
 Router.get('/agency/:agencyId', getAgency);
+Router.patch('/agency/:agencyId', approveAgency);
+Router.put('/agency/:agencyId', rejectAgency);
 
 module.exports = Router;
