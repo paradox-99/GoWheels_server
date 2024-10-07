4970b7124355b59e7925a3788b8a4f0954b80e70
Bithe <farzana.hossain5@gmail.com>
2024-10-07 19:08:01 +0600

    origin added

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const app = express();
 
 app.use(cors({
     origin: [
+        '*',
         'http://localhost:5173',
+        'http://localhost:5174',
+        "http://localhost:5175",
+
     ],
     credentials: true
 }
