c3d582c294e498595418b6d9d71262bbff619830
Bithe <farzana.hossain5@gmail.com>
2024-10-16 21:11:10 +0600

    common profile updated

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(cors({
         'http://localhost:5173',
         'http://localhost:5174',
         "http://localhost:5175",
+        "http://localhost:5176",
 
     ],
     credentials: true
