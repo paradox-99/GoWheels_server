3cc61a6c82e33030eac4b81f1ea4681854883958
Bithe <farzana.hossain5@gmail.com>
2024-10-17 13:01:58 +0600

    profile

--- routes/agencyRoutes.js
+++ routes/agencyRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const {
   showAgency,
   getAgency,
-//   agencyInfo,
+  agencyInfo,
   agencyOwnerInfo,
   updateAgencyOwnerInfo,
   addVehicleByAgency,
@@ -19,7 +19,7 @@ Router.get("/user/:email", agencyOwnerInfo);
 Router.get('/agency/:agencyId', getAgency);
 Router.patch('/agency/:agencyId', approveAgency);
 Router.put('/agency/:agencyId', rejectAgency);
-// Router.post('/agencyInfo' , agencyInfo) 
+Router.post('/agencyInfo' , agencyInfo) 
 Router.delete('/agencies/:id' , deleteAgency) 
 Router.patch('/agencyBlock/:id' , setStatus) 
 Router.get('/agency/owner/:email', agencyOwnerInfo);
