de8e0990fef90f76c5d1c0163014b60a26c5039f
Radwanul Islam Nayeem <103718355+paradox-99@users.noreply.github.com>
2024-11-03 23:35:37 +0600

    fixed a issue

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const http = require('http');
 const { app, server } = require('./app.js');  // Adjust the import to destructure properly
 const dotenv = require('dotenv');
-const socketIO = require('socket.io');
-const {server} = require('./app.js')
-
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -13,6 +9,6 @@ app.get('/', (req, res) => {
 });
 
 // Listen on the same server created in app.js
-app.listen(port, () => {
+server.listen(port, () => {
     console.log("running on port: ", port);
 });
